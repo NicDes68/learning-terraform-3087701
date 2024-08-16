d3f8adf89ae6fdb91049e0eef563e501bd313876
Nicholas <99949377+NicDes68@users.noreply.github.com>
2024-08-16 13:51:12 +0800

    Fix

--- main.tf
+++ main.tf
@@ -14,10 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default=true
-}
-
 module "blog_vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.12.1"
@@ -47,45 +43,55 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "blog-alb" {
-  source = "terraform-aws-modules/alb/aws"
-  version = "~> 9.11.0"
 
-  name    = "nhtestingalb"
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
-  target_groups = [
-      {
-        name_prefix  = "blog-"
-        protocol     = "HTTP"
-        port         = 80
-        target_type  = "instance"
-        targets = [
-          {
-            target_id = aws_instance.blog.id
-            port      = 80
-          }
-        ]
-      }
-    ]
+  name = "blog"
 
-  listeners = {
-      http={
-        port              = 80
-        protocol          = "HTTP"
-        target_group_index = 0
-      }
-  }
-
-
-
-  tags = {
-    Environment = "Development"
-  }
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
 
 
 module "blog_sg" {
@@ -100,43 +106,4 @@ module "blog_sg" {
 
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "blog" {
-	name="nh_testing"
-	description="Allow http and https in. Allow everything out"
-	vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-	type="ingress"
-	from_port=443
-	to_port=443
-	protocol="tcp"
-	cidr_blocks=["0.0.0.0/0"]
-	
-	security_group_id = aws_security_group.blog.id
-	
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-	type="ingress"
-	from_port=80
-	to_port=80
-	protocol="tcp"
-	cidr_blocks=["0.0.0.0/0"]
-	
-	security_group_id = aws_security_group.blog.id
-	
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-	type="egress"
-	from_port=0
-	to_port=0
-	protocol="-1"
-	cidr_blocks=["0.0.0.0/0"]
-	
-	security_group_id = aws_security_group.blog.id
-	
 }
