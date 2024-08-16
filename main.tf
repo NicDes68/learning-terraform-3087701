19aafc9e1b9c01aaa5f89d25fe2e32991d406e49
Nicholas <99949377+NicDes68@users.noreply.github.com>
2024-08-16 14:22:21 +0800

    Typo

--- main.tf
+++ main.tf
@@ -17,15 +17,15 @@ data "aws_ami" "app_ami" {
 module "blog_vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.12.1"
-  name= var.environment.name
-  cidr="${var.environment.network_prefix}.0.0/16"
+  name= var.Environment.name
+  cidr="${var.Environment.network_prefix}.0.0/16"
 
   azs= ["us-west-2a", "us-west-2b","us-west-2c"]
 
-  public_subnets = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
+  public_subnets = ["${var.Environment.network_prefix}.101.0/24", "${var.Environment.network_prefix}.102.0/24", "${var.Environment.network_prefix}.103.0/24"]
   tags={
     Terraform="true"
-    Environment="${var.environment.name}"
+    Environment="${var.Environment.name}"
   }
 }
 
@@ -39,7 +39,7 @@ resource "aws_instance" "blog" {
   subnet_id=module.blog_vpc.public_subnets[0]
 
   tags = {
-    Name = "${var.environment.name}"
+    Name = "${var.Environment.name}"
   }
 }
 
