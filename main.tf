0fad3320b46d80c96d324552f885e61192f01e43
Nicholas <99949377+NicDes68@users.noreply.github.com>
2024-08-16 13:08:47 +0800

    Fx

--- main.tf
+++ main.tf
@@ -56,22 +56,20 @@ module "blog-alb" {
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
-
-  target_groups = {
-    {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      targets= {
-        my_target = {
-        target_id = aws_instance.blog.id
-        port = 80 
-        }
+  target_groups = [
+      {
+        name_prefix  = "blog-"
+        protocol     = "HTTP"
+        port         = 80
+        target_type  = "instance"
+        targets = [
+          {
+            target_id = aws_instance.blog.id
+            port      = 80
+          }
+        ]
       }
-    }
-  }
-
+    ]
 
   listeners = [
       {
